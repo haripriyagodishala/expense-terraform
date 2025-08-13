a8b419ee0e54dad0a59a2d0522b5c3bd091b35f9
priya <haripriya.godishala12@gmail.com>
2025-08-13 17:14:51 +0530

    terraform

--- sg.tf
+++ sg.tf
@@ -1,9 +1,9 @@
 module "mysql_sg" {
-    source = "../terraform-aws-sg"
-    project_name = var.project_name
-    environment = var.environment
-    sg_name = "mysql"
-    vpc_id = module.vpc.vpc_id
-    common_tags = var.common_tags
-    sg_tags = var.mysql_sg_tags
+  source       = "../terraform-aws-sg"
+  project_name = var.project_name
+  environment  = var.environment
+  sg_name      = "mysql"
+  vpc_id       = module.vpc.vpc_id
+  common_tags  = var.common_tags
+  sg_tags      = var.mysql_sg_tags
 }
